3c8055867b93c511e85a6f48443404439e7d9a69
karim <karim@karim-PC>
2017-08-16 15:19:04 +0200

    .

--- Comm/src/startUp/StartUp.java
+++ Comm/src/startUp/StartUp.java
@@ -1,16 +1,5 @@
 package startUp;
 
-
-import werkzeuge.kontextWerkzeug.KontextWerkzeug;
-
-public class StartUp
-{
-
-            
-    public static void main(String[] args)
-    {
-       new KontextWerkzeug(); 
-=======
 import services.LadeServiceDummyImpl;
 import werkzeuge.kontextWerkzeug.KontextWerkzeug;
 
